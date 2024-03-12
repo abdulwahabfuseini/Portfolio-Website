510fb22f2d841f26859661767f6b2d0bb9fead66
fuseini abdul wahab <abdulwahabfuseini78@gmail.com>
2024-03-12 02:12:41 +0000

    Skills Page Updated

--- src/components/aboutMe/skills/Skills.tsx
+++ src/components/aboutMe/skills/Skills.tsx
@@ -5,7 +5,7 @@ const Skills = () => {
   return (
     <div>
       <h1 className="text-2xl sm:text-3xl">Languages and Tools</h1>
-      <div className="flex flex-wrap items-center justify-center w-full py-10 lg:px-14 gap-x-2">
+      <div className="flex flex-wrap items-center sm:justify-center w-full py-10 lg:px-14 gap-x-2">
         {skill.map((skill, index) => (
           <SkillCard key={index} icon={skill?.icon} index={index} />
         ))}
